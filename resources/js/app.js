bb264a1917a94dbae5cb17c451beb5a2750ffdd9
zaki-re <rezzoug.zak@gmail.com>
2020-11-20 21:45:48 +0100

    add vue.js files and vue router and make them working together

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,25 @@
-require('./bootstrap');
+require('./bootstrap'); 
+
+window.Vue = require('vue');
+
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+
+let routes = [
+    { path: '/dashboard', component: require('./components/Dashboard.vue').default},
+    { path: '/profile', component: require('./components/Profile.vue').default }
+  ]
+  const router = new VueRouter({
+    routes // short for `routes: routes`
+  })
+
+
+
+
+Vue.component('exemple-component',require('./components/ExempleComponent.vue'));
+const app = new Vue({
+    el: '#app',
+    router
+})
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,13 +9,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <title>AdminLTE 3 | Starter</title>
 
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+<div class="wrapper" id="app">
 
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
@@ -73,13 +74,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
                <li class="nav-item">
-                <a href="#" class="nav-link">
+                <router-link to="/dashboard" class="nav-link">
                   <i class="nav-icon fas fa-tachometer-alt"></i>
                   <p>
                    Dashboard
                     
                   </p>
-                </a>
+                </router-link>
               </li>
 
             
@@ -108,7 +109,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
 
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            <router-link to="/profile" class="nav-link">
               <i class="nav-icon fas fa-user"></i>
               <p>
                  Profile
@@ -144,8 +145,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <div class="content">
       <div class="container-fluid">
        
-     
-        <!-- /.row -->
+        <router-view></router-view>
+       
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content -->
