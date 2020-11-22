7e407e9b605a6818591aae8e253682a31fffcf73
zaki-re <rezzoug.zak@gmail.com>
2020-11-22 14:56:11 +0100

    create a user from admin dashboard with vue.js

---
+++ resources/js/components/Users.vue
@@ -0,0 +1,185 @@
+<template>
+  <div class="container">
+    <div class="row mt-5">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">Users Table</h3>
+
+            <div class="card-tools">
+              <button
+                class="btn btn-success"
+                data-toggle="modal"
+                data-target="#addNew"
+              >
+                Add new <i class="fas fa-user-plus fa-fw"></i>
+              </button>
+            </div>
+          </div>
+          <!-- /.card-header -->
+          <div class="card-body table-responsive p-0">
+            <table class="table table-hover">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Type</th>
+                  <th>Modify</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>183</td>
+                  <td>John Doe</td>
+                  <td>11-7-2014</td>
+                  <td><span class="tag tag-success">Approved</span></td>
+                  <td>
+                    <a href="#">
+                      <i class="fa fa-edit blue"></i>
+                    </a>
+                    /
+                    <a href="#">
+                      <i class="fa fa-trash red"></i>
+                    </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+      </div>
+    </div>
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="addNew"
+      tabindex="-1"
+      aria-labelledby="addNewLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="addNewLabel">Add New</h5>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+
+
+          <form @submit.prevent="createUser">
+          <div class="modal-body">
+            <div class="form-group">
+              <input
+                v-model="form.name"
+                type="text"
+                name="name"
+                placeholder="Name"
+                class="form-control"
+                :class="{ 'is-invalid': form.errors.has('name') }"
+              />
+              <has-error :form="form" field="name"></has-error>
+            </div>
+
+          
+               <div class="form-group">
+              <input
+                v-model="form.email"
+                type="text"
+                name="email"
+                placeholder="Email Adresse"
+                class="form-control"
+                :class="{ 'is-invalid': form.errors.has('email') }"
+              />
+              <has-error :form="form" field="email"></has-error>
+            </div>
+            
+            <div class="form-group">
+              <textarea
+                v-model="form.bio"
+                type="text"
+                name="bio"
+                placeholder="Short bio for the user"
+                class="form-control"
+                :class="{ 'is-invalid': form.errors.has('email') }"
+              ></textarea>
+              <has-error :form="form" field="bio"></has-error>
+            </div>
+            
+
+
+            <div class="form-group">
+              <select
+                v-model="form.type"
+                name="type"
+                id="type"
+                class="form-control"
+                :class="{ 'is-invalid': form.errors.has('type') }"
+              >
+              <option value=""> Select User Role</option>
+              <option value="admin">Admin</option>
+              <option value="user">User</option>
+              <option value="author">Author</option>  
+              </select>
+              <has-error :form="form" field="bio"></has-error>
+            </div>
+
+            <div class="form-group">
+              <input
+                v-model="form.password"
+                type="password"
+                name="password"
+                id="password"
+                class="form-control"
+                :class="{ 'is-invalid': form.errors.has('password') }"
+              />
+              <has-error :form="form" field="name"></has-error>
+            </div>
+
+
+        </div>
+        
+          <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-dismiss="modal">
+              Close
+            </button>
+            <button type="submit" class="btn btn-primary">Create</button>
+          </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      form: new Form({
+        name: "",
+        email: "",
+        password: "",
+        type: "",
+        bio: "",
+        photo: "",
+      }),
+    }
+  },
+  methods: {
+    createUser(){
+        this.form.post('api/user')
+    }
+  },
+  mounted() {
+    console.log("Component mounted.");
+  },
+};
+</script>
